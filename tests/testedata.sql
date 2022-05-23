90daedbed42542be65a4930d241fad3d483f24b0
marlonmlourenco95@gmail.com <marlonmlourenco95@gmail.com>
2022-05-23 16:50:53 +0000

    thirdcommit

--- tests/testedata.sql
+++ tests/testedata.sql
@@ -1,8 +1,10 @@
 with
     validation as (
-        select count (distinct salesorderid)
+        select count (distinct salesorderid) as sum_salesorderid
         from {{ ref ('fact_orders')}}
         where orderdate between '2013-01-01' and '2014-01-01'
     )
 
-select * from validation
+select * 
+from validation 
+where sum_salesorderid != 14244
